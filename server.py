d315711be5738ef1403d6d677dc10d2f1d32e258
Mika Mahaputra <mikamahaputra@yahoo.co.id>
2021-06-29 00:20:04 +0700

    Working on the address and connection

--- server.py
+++ server.py
@@ -9,12 +9,19 @@ server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Sending data through
 server.bind(ADDR) #Binding the socket to this address
 
 def handle_client(conn, addr):
-    pass
+    print("[NEW CONNECTION] {addr} connected.")
+
+    connected = True
+    while connected:
+        msg = conn.recv()
 
 def start():
     server.listen #Listening for new connections
     while True:
        conn, addr = server.accept() #When a new connection occurs it'll store the address of that connection
+       thread = threading.Thread(target= handle_client, args =(conn, addr))
+       thread.start()
+       print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}") #Tells how many threads are active
 
 print("[STARTING] Starting the server...")
 start()
