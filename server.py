7418c09438b3c9255df14074c8400ac50d392363
Mika Mahaputra <mikamahaputra@yahoo.co.id>
2021-06-29 00:52:26 +0700

    Finished the server codew

--- server.py
+++ server.py
@@ -1,9 +1,12 @@
 import socket 
 import threading #A way of creating a multiple thread in one python program
 
+HEADER = 64 #Sets the length of the message
 PORT = 5050 #Trying to pick a port that's not used by something else
-SERVER = socket.gethostname(socket.gethostname()) #Get the IP address automatically
+SERVER = socket.gethostbyname(socket.gethostname()) #Get the IP address automatically
 ADDR = (SERVER, PORT)
+FORMAT = "utf-8"
+DISCONNECT_MESSAGE = "!DISCONNECT"
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Sending data through stream and over the internet
 server.bind(ADDR) #Binding the socket to this address
@@ -13,15 +16,24 @@ def handle_client(conn, addr):
 
     connected = True
     while connected:
-        msg = conn.recv()
+        msg_length = conn.recv(HEADER).decode(FORMAT) #Decode from it's byte using utf-8
+        if msg_length:
+            msg_length = int(msg_length) #Converting to intreger
+            msg = conn.recv(msg_length).decode(FORMAT) #Actual message
+            if msg == DISCONNECT_MESSAGE:
+                connected = False
+        print(f"[{addr}] {msg}")
+        conn.send("Msg received".encode(FORMAT))
+    conn.close()
 
 def start():
-    server.listen #Listening for new connections
+    server.listen() #Listening for new connections
+    print(f"[LISTENING] Server is listening on {SERVER}")
     while True:
-       conn, addr = server.accept() #When a new connection occurs it'll store the address of that connection
-       thread = threading.Thread(target= handle_client, args =(conn, addr))
-       thread.start()
-       print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}") #Tells how many threads are active
+        conn, addr = server.accept() #When a new connection occurs it'll store the address of that connection
+        thread = threading.Thread(target= handle_client, args =(conn, addr))
+        thread.start()
+        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}") #Tells how many threads are active
 
 print("[STARTING] Starting the server...")
 start()
