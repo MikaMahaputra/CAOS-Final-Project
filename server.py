9e4977aa418b8c39344f775fc7c46e97af39a146
IndoG117 <55411155+IndoG117@users.noreply.github.com>
2021-06-29 11:51:17 +0700

    server done

--- server.py
+++ server.py
@@ -1,20 +1,37 @@
 import socket 
 import threading
 
+HEADER = 64 #defining header length in bytes
 PORT = 5050 # pick port
 #SERVER = "192.168.1.5" # manual own local ip definition
 SERVER = socket.gethostbyname(socket.gethostname()) # using gethostbyname to get own local ip
-print(SERVER) # debugging
+#print("server starting on address: " + SERVER) # debugging
 ADDR = (SERVER, PORT) # socket address
+FORMAT = 'utf-8' #formatter
+DISCONNECT_MESSAGE = "!DISCONNECT"
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # socket creation and type
 server.bind(ADDR) # binding socket
 
-def handle_client(conn, addr):
-    pass
 
-def start():
+def handle_client(conn, addr): #client handler function
+    print(f"[NEW CONNECTION] {addr} connected.")
+
+    connected = True
+    while connected:
+        msg_length = conn.recv(HEADER).decode(FORMAT) #decoding the message into readable form by program
+        msg_length = int(msg_length) #turning message length into an int
+        msg = conn.recv(msg_length).decode(FORMAT) #decoding actual message
+        if msg == DISCONNECT_MESSAGE: #if client disconnects
+            connected = False
+        
+        print(f"[{addr}] {msg}")
+
+    conn.close() #terminates connection
+
+def start(): #server starter function
     server.listen() #listen for new connections
+    print(f"[LISTENING] Server is listening on {SERVER}")
     while True:
         conn, addr = server.accept() #get the address of the connection, conn to store info about the client
         thread = threading.Thread(target=handle_client, args=(conn, addr))# new thread to run handle_client
