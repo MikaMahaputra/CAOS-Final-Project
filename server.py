c55967bf01c72872ecb6b119d9c86c0f2d37956f
IndoG117 <55411155+IndoG117@users.noreply.github.com>
2021-06-29 11:29:53 +0700

    coded in multithreading for new client connections

--- server.py
+++ server.py
@@ -10,7 +10,17 @@ ADDR = (SERVER, PORT) # socket address
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # socket creation and type
 server.bind(ADDR) # binding socket
 
+def handle_client(conn, addr):
+    pass
 
+def start():
+    server.listen() #listen for new connections
+    while True:
+        conn, addr = server.accept() #get the address of the connection, conn to store info about the client
+        thread = threading.Thread(target=handle_client, args=(conn, addr))# new thread to run handle_client
+        thread.start()
+        print(f"[ACTIVE CONNECTIONS {threading.activeCount() - 1}") # counts number of threads per new client so we also know number of clients from number of threads  (-1 to discount the start thread)
 
-
+print("[STARTING] server is starting...")
+start()
 
