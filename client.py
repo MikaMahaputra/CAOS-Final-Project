03f19cf2b68ebfd658f717d5881336653b3566bf
IndoG117 <55411155+IndoG117@users.noreply.github.com>
2021-06-29 13:41:35 +0700

    tidied some things

--- client.py
+++ client.py
@@ -1,20 +1,20 @@
 import socket
 
 HEADER = 64
-PORT = 6050
-FORMAT = 'utf-8'
-DISCONNECT_MESSAGE = "!DISCONNECT"
-SERVER = "172.20.160.1"
+PORT = 6050 #target port
+FORMAT = 'utf-8' #message formatting
+DISCONNECT_MESSAGE = "!DISCONNECT" #disconnect message
+SERVER = "172.20.160.1" #target ip addess
 ADDR = (SERVER, PORT)
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # socket type
 client.connect(ADDR)
 
-def send(msg):
-    message = msg.encode(FORMAT)
+def send(msg): #message sender
+    message = msg.encode(FORMAT) #encode messsage into utf
     msg_length = len(message)
     send_length = str(msg_length).encode(FORMAT)
-    send_length += b' ' * (HEADER - len(send_length)) # padding
+    send_length += b' ' * (HEADER - len(send_length)) # padding to 64 for validation
     client.send(send_length)
     client.send(message)
     print(client.recv(2048).decode(FORMAT)) # to recieve message from server
